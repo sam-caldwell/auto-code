4c1f16defd6355e02ef7052f7f498b0add92a342
Sam Caldwell <github@samcaldwell.net>
2024-10-30 04:15:29 -0500

    update docs for ParameterDefinition

---
+++ ParameterDefinition.go
@@ -0,0 +1,33 @@
+package arguments
+
+// ParameterDefinition - a configuration parameter definition.
+//
+//	The ParameterDefinition object defines the parameter, providing a helpful HelpText object, a Value object pointer
+//	and a DataSourceCollection from which the parameter's actual value can be obtained.
+//
+//	A ParameterDefinition assumes that--
+//	   - HelpText was created with NewHelpText(),
+//	   - Value (pointer) is created with NewValue()
+//	   - DataSourceCollection is created with NewDataSourceCollection()
+//	since these functions perform data validation.
+//
+//	ParameterDefinition
+//	  │
+//	  ├── helpText:  HelpText
+//	  ├── value: *Value
+//	  └── sources: DataSourceCollection
+type ParameterDefinition struct {
+	helpText HelpText
+	value    *Value
+	sources  DataSourceCollection // order must be preserved!
+}
+
+// HelpText - Return the HelpText as a string
+func (p *ParameterDefinition) HelpText() string {
+	return string(p.helpText)
+}
+
+// SourceCount - Return the number of data sources
+func (p *ParameterDefinition) SourceCount() int {
+	return p.sources.Count()
+}
