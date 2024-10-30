32b90d6b3d164147a075996a359402240063344e
Sam Caldwell <github@samcaldwell.net>
2024-10-30 17:36:49 -0500

    fixes for NewParameterCollection()

--- NewParameterCollection.go
+++ NewParameterCollection.go
@@ -1,11 +1,25 @@
 package arguments
 
 // NewParameterCollection - initializes and returns a new ParameterCollection.
-func NewParameterCollection(name, help string, value *Value, sources []*DataSource) (ParameterCollection, error) {
-	pName, err := NewParameterName(name)
-	pHelp, err := NewHelpText
+func NewParameterCollection(name, help string, value ValueDataInterface,
+	sources []*DataSource) (p ParameterCollection, err error) {
 
-	p := make(ParameterCollection)
-	p.add(name, helpText, value, sources)
-	return p, nil
+	p = make(ParameterCollection)
+
+	pName, err := NewParameterName(name)
+	if err != nil {
+		return p, err
+	}
+	pHelp, err := NewHelpText(&help)
+	if err != nil {
+		return p, err
+	}
+
+	parameterDefinition, err := NewParameterDefinition(pHelp, value, sources)
+	if err != nil {
+		return p, err
+	}
+
+	err = p.add(pName, pHelp, parameterDefinition)
+	return p, err
 }
