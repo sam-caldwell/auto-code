9cf79fea1979a15c9e8083342438c8d008cdc729
Sam Caldwell <github@samcaldwell.net>
2024-10-30 17:01:07 -0500

    fix NewParser()

--- NewParser.go
+++ NewParser.go
@@ -1,8 +1,5 @@
 package arguments
 
-func New() *Argument {
-	return &Argument{
-		err:  nil,
-		args: make(map[string]*ArgumentDescriptor[int64, uint64, float64, string, bool]),
-	}
+func NewParser() *Parser {
+	return &Parser{}
 }
