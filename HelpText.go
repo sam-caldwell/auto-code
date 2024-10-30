7763d65e2226e27272b00c42e2098f008ddb8010
Sam Caldwell <github@samcaldwell.net>
2024-10-30 00:36:13 -0500

    code cleanup

--- HelpText.go
+++ HelpText.go
@@ -2,15 +2,3 @@ package arguments
 
 // HelpText - A type representing a parameter's help text string
 type HelpText string
-
-// String - Return the string version of a help text string
-func (text *HelpText) String() string {
-	return string(*text)
-}
-
-// set - A sanitizer/setter for the help text string.
-func (text *HelpText) set(helpText *string) error {
-	//ToDo: add validation
-	*text = HelpText(*helpText)
-	return nil
-}
