1d0fd44d10851d8e866791537e278e29c5a649f2
Sam Caldwell <github@samcaldwell.net>
2024-10-30 04:13:01 -0500

    code cleanup

--- validator.go
+++
@@ -1,7 +0,0 @@
-package arguments
-
-// Number - constraint to support multiple numeric types
-type Number interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 |
-		uint16 | uint32 | uint64 | float32 | float64
-}
