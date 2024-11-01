55c246379279871378bbba57c13a8d9c22dcf813
Sam Caldwell <github@samcaldwell.net>
2024-11-01 17:25:46 -0500

    add docstrings to Manifest struct

--- manifest/Manifest.go
+++ manifest/Manifest.go
@@ -2,7 +2,10 @@ package manifest
 
 // Manifest represents the YAML manifest structure for code generation
 type Manifest struct {
+
+	// Global - defines the Global section representing general program parameters
 	Global GlobalSection `yaml:"global"`
 
+	// Config - defines the Config section representing the configuration feature
 	Config ConfigSection `yaml:"config"`
 }
