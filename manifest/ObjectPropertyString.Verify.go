ec2f5fdd9af625c08dc6324780950a4c7c629a6f
Sam Caldwell <github@samcaldwell.net>
2024-11-01 08:41:22 -0500

    add ObjectPropertyString.Verify()

--- manifest/ObjectPropertyString.Verify.go
+++ manifest/ObjectPropertyString.Verify.go
@@ -1,6 +1,19 @@
 package manifest
 
+import (
+	"fmt"
+	"regexp"
+)
+
 // Verify - Validate the ObjectPropertyString
 func (objectPropertyString *ObjectPropertyString) Verify() error {
-	return nil
+
+	if pattern := regexp.MustCompile(objectPropertyStringPattern); pattern.MatchString(string(*objectPropertyString)) {
+
+		return nil
+
+	}
+
+	return fmt.Errorf(errInvalidObjectPropertyString, *objectPropertyString)
+
 }
