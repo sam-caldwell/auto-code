99dbc17cce0d0ef393320790be3f7e08182fec9d
Sam Caldwell <github@samcaldwell.net>
2024-11-01 16:48:27 -0500

    fixes for GitRepoUrlString.Set()

--- manifest/GitRepoUrlString.Set.go
+++ manifest/GitRepoUrlString.Set.go
@@ -1,10 +1,19 @@
 package manifest
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Set - Set the local git repo url using the given string
 func (repo *GitRepoUrlString) Set(value string) error {
 
+	if strings.TrimSpace(value) == EmptyString {
+		return fmt.Errorf(errMissingLocalGitRepoUrl)
+	}
+
 	*repo = GitRepoUrlString(value)
 
-	return repo.Verify()
+	return nil
 
 }
