8c3c418b62b91c83e4a7f0ed39944779098d8cd9
Sam Caldwell <github@samcaldwell.net>
2024-11-01 16:46:42 -0500

    code cleanup

--- manifest/GlobalSection.Verify.go
+++ manifest/GlobalSection.Verify.go
@@ -6,20 +6,14 @@ func (global GlobalSection) Verify() error {
 	vTable := []func() error{
 
 		global.Name.Verify,
-
 		global.Description.Verify,
-
 		global.Author.Verify,
-
 		global.Email.Verify,
-
 		global.Copyright.Verify,
-
 		global.License.Verify,
-
 		global.Language.Verify,
-
 		global.Version.Verify,
+		global.GitRepoUrl.Verify,
 
 		//ToDo: If you add anything to the global section, add a function to this table to verify its content.
 
