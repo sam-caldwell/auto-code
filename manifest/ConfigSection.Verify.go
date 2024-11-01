3d0edebe83883d891f5e17763fa023cf29178943
Sam Caldwell <github@samcaldwell.net>
2024-11-01 06:25:22 -0500

    fixes for ConfigSection.Verify()

--- manifest/ConfigSection.Verify.go
+++ manifest/ConfigSection.Verify.go
@@ -3,11 +3,11 @@ package manifest
 // Verify - verify the config section of a manifest.yaml file
 func (config *ConfigSection) Verify() error {
 	vTable := []func() error{
-		config.verifyConfigCommandline,
-		config.verifyConfigEnvironment,
-		config.verifyConfigFile,
-		config.Config.Sources.Verify,
-		config.verifyConfigProperties,
+		config.Commandline.Verify,
+		config.Environment.Verify,
+		config.File.Verify,
+		config.Sources.Verify,
+		config.Properties.Verify,
 		//ToDo: If you add anything to the config section, add a function to this table to verify its content.
 		//      the order of verification functions does not matter.  We probably should keep it alphabetical.
 	}
