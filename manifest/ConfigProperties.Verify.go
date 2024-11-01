c10ea95978141fb977519b2f0af37f4aa86f6053
Sam Caldwell <github@samcaldwell.net>
2024-11-01 04:32:28 -0500

    refactor ConfigProperties type and Verify() methods

---
+++ manifest/ConfigProperties.Verify.go
@@ -0,0 +1,30 @@
+package manifest
+
+import (
+	"fmt"
+)
+
+// Verify - verify that config.properties is properly formatted
+func (properties ConfigProperties) Verify() error {
+
+	if len(properties) < 1 {
+
+		return fmt.Errorf(errMissingConfigProperties)
+
+	}
+
+	for propertyName, property := range properties {
+
+		if err := propertyName.Verify(); err != nil {
+			return err
+		}
+
+		if err := property.Verify(&propertyName); err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+
+}
---
+++ manifest/ConfigProperties.go
@@ -0,0 +1,4 @@
+package manifest
+
+// ConfigProperties - A map of property names to its property descriptor
+type ConfigProperties map[PropertyName]ConfigProperty
