53f585cee160450be19e7098b09b6324f2809879
Sam Caldwell <github@samcaldwell.net>
2024-11-01 17:26:42 -0500

    update main() - wip

--- main.go
+++ main.go
@@ -2,24 +2,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	generator "github.com/sam-caldwell/auto-code/generator"
-	manifest2 "github.com/sam-caldwell/auto-code/manifest"
-	"log"
+	"github.com/sam-caldwell/ansi"
+	"github.com/sam-caldwell/auto-code/generator"
+	"github.com/sam-caldwell/auto-code/manifest"
+	"github.com/sam-caldwell/file"
+)
+
+const (
+	defaultManifest   = "manifest.yaml"
+	manifestFlagUsage = "Identify the project descriptor file"
+	manifestFlag      = "--manifest"
+	defaultNoop       = false
+	noopUsage         = "do not generate code artifacts"
+	noopFlag          = "--noop"
+	codeRootFlag      = "--projectRoot"
+	defaultCodeRoot   = "./"
+	codeRootUsage     = "the root directory for the project (where the generated code will be written)"
+	errorExitCode     = 1
 )
 
 func main() {
+	var projectManifest manifest.Manifest
+	manifestFileName := flag.String(manifestFlag, defaultManifest, manifestFlagUsage)
+	noop := flag.Bool(noopFlag, defaultNoop, noopUsage)
+	codeRoot := flag.String(codeRootFlag, defaultCodeRoot, codeRootUsage)
+	flag.Parse()
 
-	manifest, err := manifest2.LoadManifest("manifest.yaml")
-
-	if err != nil {
-		log.Fatal("Failed to load manifest:", err)
+	if !file.Exists(*manifestFileName) {
+		ansi.Red().Printf("manifest file (%s) not found\n", *manifestFileName).Fatal(errorExitCode)
 	}
 
-	err = generator.GenerateCode(manifest)
+	if err := projectManifest.Load("manifest.yaml"); err != nil {
+		ansi.Red().Printf("Failed to load manifest: '%s'\n", err).Fatal(errorExitCode)
+	}
 
-	if err != nil {
-		log.Fatal("Code generation failed:", err)
+	if err := generator.Run(&projectManifest, codeRoot, *noop); err != nil {
+		ansi.Red().Printf("Failed to generate code: '%s'\n", err).Fatal(errorExitCode)
 	}
 
 	fmt.Println("Code generation complete!")
