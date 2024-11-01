996dcd40beefb571b0056084b47e5ded3b005148
Sam Caldwell <github@samcaldwell.net>
2024-11-01 04:42:07 -0500

    add PropertyType type and its Verify() method

---
+++ manifest/PropertyType.Verify.go
@@ -0,0 +1,26 @@
+package manifest
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Verify - Validate the PropertyType and ensure it is lowercase and has no leading or trailing whitespace.
+func (propertyType *PropertyType) Verify(name *PropertyName) error {
+
+	for _, pType := range strings.Split(configPropertyTypes, Comma) {
+
+		// strip the property type and make sure it is lower case.
+		*propertyType = PropertyType(strings.ToLower(string(*propertyType)))
+
+		if pType == string(*propertyType) {
+
+			return nil
+
+		}
+
+	}
+
+	return fmt.Errorf(errUnsupportedPropertyType, *name)
+
+}
---
+++ manifest/PropertyType.go
@@ -0,0 +1,4 @@
+package manifest
+
+// PropertyType - a string representing a supported data type for property objects.
+type PropertyType string
