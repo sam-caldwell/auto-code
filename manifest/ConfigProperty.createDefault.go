0ad768d793a24145cb302e5d71f518166bde5e09
Sam Caldwell <github@samcaldwell.net>
2024-11-01 02:55:52 -0500

    add ConfigProperty.createDefault()

---
+++ manifest/ConfigProperty.createDefault.go
@@ -0,0 +1,37 @@
+package manifest
+
+// createDefault - return a default value of the correct type.
+func (property *ConfigProperty) createDefault() any {
+	switch property.Type {
+	case "bool":
+		return false
+	case "int":
+		return int(0)
+	case "int8":
+		return int8(0)
+	case "int16":
+		return int16(0)
+	case "int32":
+		return int32(0)
+	case "int64":
+		return int64(0)
+	case "uint":
+		return uint(0)
+	case "uint8":
+		return uint8(0)
+	case "uint16":
+		return uint16(0)
+	case "uint32":
+		return uint32(0)
+	case "uint64":
+		return uint64(0)
+	case "float32":
+		return float32(0)
+	case "float64":
+		return float64(0)
+	case "string":
+		return EmptyString
+	default:
+		return nil
+	}
+}
