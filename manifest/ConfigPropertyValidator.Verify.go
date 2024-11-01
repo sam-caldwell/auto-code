0730ab43a20da9cf6b671364775cb8013552a733
Sam Caldwell <github@samcaldwell.net>
2024-11-01 05:08:47 -0500

    add configPropertyValidator type and Verify() method

--- manifest/ConfigPropertyValidator.Verify.go
+++ manifest/ConfigPropertyValidator.Verify.go
@@ -1,28 +1,33 @@
 package manifest
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// VerifyValidator - Verify the property default value to ensure it is supported.
-func (property *ConfigProperty) VerifyValidator(name *string) error {
+// Verify - Validate that the property validator object is appropriate for its associated property.
+func (validator ConfigPropertyValidator) Verify(name *PropertyName, property *ConfigProperty) error {
 
-	switch class := property.Validator.Class; class {
+	switch class := validator.Class; class {
 	case "minmax":
+
 		if property.isPropertyTypeNumeric() {
 			return verifyMinMax(name, property)
 		}
 		return fmt.Errorf(errTypeMismatchNumeric, *name)
+
 	case "null":
+
 		if property.Validator.Parameter != nil {
 			return fmt.Errorf(errExpectedEmptyValidatorParameter, *name)
 		}
+
 	case "regex":
+
 		if property.Type != "string" {
 			return fmt.Errorf(errTypeMismatchString, *name)
 		}
 		return property.Validator.verifyRegex(name)
+
 	}
+
 	return fmt.Errorf(errUnsupportedPropertyValidator, *name)
 
 }
