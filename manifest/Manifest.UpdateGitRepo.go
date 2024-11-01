8867bbb4498e9b3c946f01caa7bb4e3af6bca795
Sam Caldwell <github@samcaldwell.net>
2024-11-01 17:14:42 -0500

    fixes for Manifest.UpdateGitRepo()

--- manifest/Manifest.UpdateGitRepo.go
+++ manifest/Manifest.UpdateGitRepo.go
@@ -5,35 +5,47 @@ import (
 )
 
 // UpdateGitRepo - merge the manifest.Global.git_repo and local git repo and ensure agreement
+//
+// We are going to merge global.git_repo from the manifest YAML file with the local git repo
+// and verify the two agree. In an optimal state
 func (manifest *Manifest) UpdateGitRepo() (err error) {
 
+	// get the local git repo url
 	var localRepo GitRepoUrlString
-	if localRepo, err = gitLocalRepo(); err != nil {
-
+	if localRepo, err = manifest.gitLocalRepo(); err != nil {
+		// if the manifest does not have a git_repo url, we cannot go forward.
 		if manifest.Global.GitRepoUrl == EmptyString {
-
-			// We have no means of recovery.  There is no local repo and no repo in the manifest yaml file.
-			return fmt.Errorf(errMissingGitRepoUrl)
-
-		} else {
-
-			// Local repo is not defined.  But the manifest yaml defines a git repo.
-			// We can initialize the local git repo at this point.
-			if err := manifest.Global.GitRepoUrl.Init(); err != nil {
-				return err
-			}
-
-			// Verify the local repo initialization was correct.
-			if localRepo, err = gitLocalRepo(); err != nil {
-				return fmt.Errorf(errLocalRepoInitFailed, err)
-			}
-
+			// the local git repo could not be loaded or was not defined.
+			return fmt.Errorf(errMissingGitRepoUrl, err)
 		}
-		return err
+
+		// if the manifest has a git_repo url, we can set our local repo to that value.
+		if err = manifest.Global.GitRepoUrl.Init(); err != nil {
+			// on error, bail.  We cannot fix the local repo, so there is an unrecoverable problem.
+			return err
+		}
+
+		// now we can load the local repo again to verify our errors are resolved.
+		if localRepo, err = manifest.gitLocalRepo(); err != nil {
+			return err // We have proven we have an unrecoverable error
+		}
+	} else {
+		// localRepo was loaded without error.
+		if manifest.Global.GitRepoUrl == EmptyString {
+			// the local git repo could not be loaded or was not defined.
+			return fmt.Errorf(errMissingGitRepoUrl, err)
+		} else {
+			// we had no manifest.Global.git_repo in the manifest, but we had a localRepo
+			// so we will set our manifest version to the localRepo value.
+			manifest.Global.GitRepoUrl = localRepo
+		}
 	}
 
-	if repo := string(manifest.Global.GitRepoUrl); repo == EmptyString {
-		//repo is an empty string, we will need to set the value to the local repo.
+	// Check final agreement to ensure local and manifest git repo are the same.
+	if localRepo != manifest.Global.GitRepoUrl {
+		return fmt.Errorf(errGitRepoUrlMismatch)
 	}
+
+	return err
 
 }
