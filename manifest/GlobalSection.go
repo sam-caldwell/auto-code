589f4e52fa91ac7c9c69ed01a2d7dfaf14be0df3
Sam Caldwell <github@samcaldwell.net>
2024-11-01 03:45:11 -0500

    code cleanup

--- manifest/GlobalSection.go
+++ manifest/GlobalSection.go
@@ -1,12 +1,19 @@
 package manifest
 
 type GlobalSection struct {
-	Name        string `yaml:"name"`
+	Name string `yaml:"name"`
+
 	Description string `yaml:"description"`
-	Author      string `yaml:"author"`
-	Email       string `yaml:"email"`
-	Copyright   string `yaml:"copyright"`
-	License     string `yaml:"license"`
-	Language    string `yaml:"language"`
-	Version     string `yaml:"version"`
+
+	Author string `yaml:"author"`
+
+	Email string `yaml:"email"`
+
+	Copyright string `yaml:"copyright"`
+
+	License string `yaml:"license"`
+
+	Language string `yaml:"language"`
+
+	Version string `yaml:"version"`
 }
