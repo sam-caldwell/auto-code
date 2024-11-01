13563e20469bfe0e79714460a94069bb0d86ff80
Sam Caldwell <github@samcaldwell.net>
2024-11-01 05:23:11 -0500

    fixes for ConfigProperty.Verify()

---
+++ manifest/ConfigProperty.Verify.go
@@ -0,0 +1,26 @@
+package manifest
+
+// Verify - verify a given element of config.properties[]
+func (property *ConfigProperty) Verify(name *PropertyName) error {
+
+	if err := property.Type.Verify(name); err != nil {
+
+		return err
+
+	}
+
+	if err := property.Validator.Verify(name, property); err != nil {
+
+		return err
+
+	}
+
+	if err := property.Type.verifyDefault(name, &property.Default); err != nil {
+
+		return err
+
+	}
+
+	return nil
+
+}
