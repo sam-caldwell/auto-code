1e36b1a02eccdc37c83845239d4bc9c844bdd016
Sam Caldwell <github@samcaldwell.net>
2024-11-01 06:50:45 -0500

    add FileNameString.Verify()

---
+++ manifest/FileNameString.Verify.go
@@ -0,0 +1,14 @@
+package manifest
+
+import (
+	"fmt"
+	"regexp"
+)
+
+// Verify - validate a given Filename is properly formatted.
+func (name FileNameString) Verify() error {
+	if pattern := regexp.MustCompile(fileNameStringPattern); pattern.MatchString(string(name)) {
+		return nil
+	}
+	return fmt.Errorf(errInvalidFileName, name)
+}
