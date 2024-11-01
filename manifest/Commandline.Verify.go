a9407b6f68526d36ef8415fd001f9def0dc7e437
Sam Caldwell <github@samcaldwell.net>
2024-11-01 05:55:53 -0500

    WIP: Commandline

---
+++ manifest/Commandline.Verify.go
@@ -0,0 +1,6 @@
+package manifest
+
+func (command *Commandline) Verify() error {
+	//ToDo: complete this to verify the object...
+	return nil
+}
--- manifest/Commandline.go
+++ manifest/Commandline.go
@@ -1,8 +1,8 @@
 // Package manifest generator/Manifest.go
 package manifest
 
-// ConfigCommandline maps CLI arguments to properties
-type ConfigCommandline struct {
+// Commandline - maps CLI arguments to properties
+type Commandline struct {
 	Property string `yaml:"property"`
 	Required bool   `yaml:"required"`
 	Short    string `yaml:"short"`
