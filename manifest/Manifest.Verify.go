8a8ae4ca1563f0434b5f5a1d4aca4cab76afa69e
Sam Caldwell <github@samcaldwell.net>
2024-11-01 03:44:31 -0500

    fix docstring for Manifest.Verify()

--- manifest/Manifest.Verify.go
+++ manifest/Manifest.Verify.go
@@ -1,16 +1,21 @@
 // Package manifest generator/Manifest.go
 package manifest
 
-// Verify - Verify the manifest.yaml file contains valid data.
+// Verify - Verify the top-level of the manifest.yaml configuration file
 func (manifest *Manifest) Verify() error {
+
 	if err := manifest.Global.Verify(); err != nil {
 		return err
 	}
+
 	if err := manifest.Config.Verify(); err != nil {
 		return err
 	}
+
 	//
 	// ToDo: Extend with other top-level manifest.yaml sections
 	//
+
 	return nil
+
 }
