51682e6a0cb446717e468d128e8dee9032d69964
Sam Caldwell <github@samcaldwell.net>
2024-11-01 17:41:23 -0500

    fix type passed to MatchString() in verifyGlobalLicense()

--- manifest/Manifest.verifyGlobalLicense.go
+++ manifest/Manifest.verifyGlobalLicense.go
@@ -7,8 +7,11 @@ import (
 
 // verifyGlobalLicense - verify global.license from manifest.yaml
 func (manifest *Manifest) verifyGlobalLicense() error {
-	if pattern := regexp.MustCompile(globalLicensePattern); pattern.MatchString(manifest.Global.Name) {
+
+	if pattern := regexp.MustCompile(globalLicensePattern); pattern.MatchString(string(manifest.Global.Name)) {
 		return nil
 	}
+
 	return fmt.Errorf(errInvalidLicensePattern, globalLicensePattern)
+
 }
