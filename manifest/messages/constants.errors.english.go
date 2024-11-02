2a36217b51e5fe9d6ab9864c6daed73727e917a9
Sam Caldwell <github@samcaldwell.net>
2024-11-02 01:48:06 -0500

    update messages

--- manifest/messages/constants.errors.english.go
+++ manifest/messages/constants.errors.english.go
@@ -3,39 +3,41 @@ package messages
 //ToDo: add build configs so this only builds if english is specified to allow language portability
 
 const (
-	errDefaultValueTypeMismatch        = `config.property.Type must match config.property.default (property:%s)`
-	errDuplicateConfigSource           = `config.sources cannot contain duplicate elements`
-	errEmptyGitRepoUrl                 = `empty git repo url`
+	ErrDefaultValueTypeMismatch        = `config.property.Type must match config.property.default (property:%s)`
+	ErrDuplicateConfigSource           = `config.sources cannot contain duplicate elements`
+	ErrEmptyGitRepoUrl                 = `empty git repo url`
 	ErrEmptyRegularExpression          = `empty regular expression is not allowed`
-	errExpectedEmptyValidatorParameter = `config.properties[].validator should be nil if class is null (property:%s)`
-	errExpectedRegexString             = `expected regular expression string (property: %s)`
-	errGitInitFailed                   = `'git init' failed: %v`
-	errGitRemoteAddFailed              = `'git remote add' failed: %v`
-	errInvalidConfigSource             = `unsupported or unknown config.source value (expect: %s)`
-	errInvalidCopyrightPattern         = `global copyright string must be properly formatted`
-	errInvalidEnvironmentVariableName  = `invalid environment variable name (%s)`
-	errInvalidEmailAddress             = `email address must be valid`
-	errInvalidFileName                 = `invalid file name (%s)`
-	errInvalidFileFormat               = `invalid file format (%s)`
-	errInvalidGitRepoUrl               = `invalid git url (%s)`
-	errInvalidLanguagePattern          = `language must be supported by auto-code`
-	errInvalidLicensePattern           = `license must match pattern (%s)`
-	errInvalidMinMaxValue              = `min/max values must be numeric where min<max`
-	errInvalidNonEmptyText             = `a non-empty/whitespace text string is expected`
-	errInvalidNameIdentifier           = `name must match pattern (%s)`
-	errInvalidObjectPropertyString     = `invalid object property string (%s)`
-	errInvalidLongArgument             = `long argument '%s' is not valid`
-	errInvalidShortArgument            = `short argument '%s' is not valid`
-	errInvalidValidatorRegex           = `invalid validator regular expression (property: %s, regex: %s, err: %v)`
-	errInvalidVersionPattern           = `version must match pattern (%s)`
-	errGitRepoUrlMismatch              = `git repo url mismatch (local != remote)`
-	errMissingCommandlineArgument      = `missing argument: must have either short, long or both argument type`
-	errMissingConfigProperties         = `config.properties must have at least one element`
-	errMissingConfigSource             = `config.sources must have at least one element`
-	errMissingLocalGitRepoUrl          = `missing GitRepoUrl (git remote -v)`
-	errMissingGitRepoUrl               = `missing local repo and manifest (global.git_repo): '%v'`
-	errGitRemoteUrlFailed              = `'git remote -v' failed: 'Error:%v'`
-	errUnknownProperty                 = `unknown property (%s)`
-	errUnsupportedPropertyType         = `unsupported property type for '%s'`
-	errUnsupportedPropertyValidator    = `unsupported property validator for '%s'`
+	ErrExpectedEmptyValidatorParameter = `config.properties[].validator should be nil if class is null (property:%s)`
+	ErrExpectedRegexString             = `expected regular expression string (property: %s)`
+	ErrGitInitFailed                   = `'git init' failed: %v`
+	ErrGitRemoteAddFailed              = `'git remote add' failed: %v`
+	ErrInvalidConfigSource             = `unsupported or unknown config.source value (expect: %s)`
+	ErrInvalidCopyrightPattern         = `global copyright string must be properly formatted`
+	ErrInvalidEnvironmentVariableName  = `invalid environment variable name (%s)`
+	ErrInvalidEmailAddress             = `email address must be valid`
+	ErrInvalidFileName                 = `invalid file name (%s)`
+	ErrInvalidFileFormat               = `invalid file format (%s)`
+	ErrInvalidGitRepoUrl               = `invalid git url (%s)`
+	ErrInvalidLanguagePattern          = `language must be supported by auto-code`
+	ErrInvalidLicensePattern           = `license must match pattern (%s)`
+	ErrInvalidMinMaxValue              = `min/max values must be numeric where min<max`
+	ErrInvalidNonEmptyText             = `a non-empty/whitespace text string is expected`
+	ErrInvalidNameIdentifier           = `name must match pattern (%s)`
+	ErrInvalidObjectPropertyString     = `invalid object property string (%s)`
+	ErrInvalidLongArgument             = `long argument '%s' is not valid`
+	ErrInvalidShortArgument            = `short argument '%s' is not valid`
+	ErrInvalidValidatorRegex           = `invalid validator regular expression (property: %s, regex: %s, err: %v)`
+	ErrInvalidVersionPattern           = `version must match pattern (%s)`
+	ErrGitRepoUrlMismatch              = `git repo url mismatch (local != remote)`
+	ErrMissingCommandlineArgument      = `missing argument: must have either short, long or both argument type`
+	ErrMissingConfigProperties         = `config.properties must have at least one element`
+	ErrMissingConfigSource             = `config.sources must have at least one element`
+	ErrMissingLocalGitRepoUrl          = `missing GitRepoUrl (git remote -v)`
+	ErrMissingGitRepoUrl               = `missing local repo and manifest (global.git_repo): '%v'`
+	ErrGitRemoteUrlFailed              = `'git remote -v' failed: 'Error:%v'`
+	ErrTypeMismatchMin                 = `type mismatch(Min)`
+	ErrTypeMismatchMax                 = `type mismatch(Max)`
+	ErrUnknownProperty                 = `unknown property (%s)`
+	ErrUnsupportedPropertyType         = `unsupported property type for '%s'`
+	ErrUnsupportedPropertyValidator    = `unsupported property validator for '%s'`
 )
