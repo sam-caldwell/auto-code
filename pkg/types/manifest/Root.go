a909ef050d6495a4e7b56ac103a3f159f88f0342
Sam Caldwell <github@samcaldwell.net>
2024-11-18 13:40:32 -0600

    cleanup

--- pkg/types/manifest/Root.go
+++ pkg/types/manifest/Root.go
@@ -1,6 +1,10 @@
 package manifest
 
 // Root - represents the top-level of the auto-code YAML specification
+//
+// Note: The document root does not support $ref because this would be
+//
+//	confusing.
 type Root struct {
 	Version       SemanticVersion `yaml:"auto-code"`
 	Info          Info            `yaml:"info"`
