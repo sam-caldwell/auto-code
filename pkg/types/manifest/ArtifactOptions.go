faa7a6ddb4afa68f5dbb068866648165d23f3540
Sam Caldwell <github@samcaldwell.net>
2024-11-18 13:39:52 -0600

    refactor ArtifactOptions to handle

--- pkg/types/manifest/ArtifactOptions.go
+++ pkg/types/manifest/ArtifactOptions.go
@@ -1,6 +1,8 @@
 package manifest
 
+// ArtifactOptions - represents the options associated with a project artifact
 type ArtifactOptions struct {
+	DataObjectWithReference
 	Language  Language            `yaml:"language,omitempty"`
 	Sources   PosixDirectory      `yaml:"sources,omitempty"`
 	Url       UrlPattern          `yaml:"url,omitempty"`
