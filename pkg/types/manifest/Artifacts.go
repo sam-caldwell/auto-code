2fca34be54bcc03acbfab2dddf2ad0d28efe6a79
Sam Caldwell <github@samcaldwell.net>
2024-11-18 13:40:13 -0600

    cleanup

--- pkg/types/manifest/Artifacts.go
+++ pkg/types/manifest/Artifacts.go
@@ -1,5 +1,8 @@
 package manifest
 
+// Artifacts - represents the collection of artifacts in a solution
+//
+// Note: the artifacts object does NOT support $ref, though its children do.
 type Artifacts struct {
 	Artifact []ArtifactDescriptor
 }
