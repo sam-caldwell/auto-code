b13a8a10240fa0cf51f19793f21858d8b8e70d68
Sam Caldwell <github@samcaldwell.net>
2024-11-19 03:27:50 -0600

    add DataContractDescriptor

--- pkg/types/manifest/DataContractDescriptor.go
+++ pkg/types/manifest/DataContractDescriptor.go
@@ -3,7 +3,7 @@ package manifest
 // DataContractDescriptor - represents the content of a data contract
 type DataContractDescriptor struct {
 	DataObjectWithReference
-	Name      NonEmptyString       `yaml:"name"`
-	Artifacts []ArtifactName       `yaml:"artifacts"` // Artifacts - Map the data contract to one or more artifacts
-	Schema    DataSchemaDescriptor `yaml:"schema"`
+	Name      NonEmptyString        `yaml:"name"`
+	Artifacts []ArtifactName        `yaml:"artifacts"`
+	Schema    DataSchemaDescriptors `yaml:"schema"`
 }
