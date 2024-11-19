21ec25721b82fa2cfa5f8afbd203c461fda97e9c
Sam Caldwell <github@samcaldwell.net>
2024-11-19 03:30:27 -0600

    add DataSchemaPostgres to implement DataSchemaDescriptors interface

--- pkg/types/manifest/DataSchemaPostgres.go
+++ pkg/types/manifest/DataSchemaPostgres.go
@@ -2,7 +2,8 @@ package manifest
 
 // DataSchemaPostgres - An implementation of DataSchemaDescriptor used for Postgresql Db
 type DataSchemaPostgres struct {
-	Driver     DataDriver          `yaml:"driver"`
+	DataSchemaDescriptorBase
+
 	Tables     []PostgresTable     `yaml:"tables,omitempty"`
 	Views      []PostgresView      `yaml:"views,omitempty"`
 	Functions  []PostgresFunction  `yaml:"functions,omitempty"`
