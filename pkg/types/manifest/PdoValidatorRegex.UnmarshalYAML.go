714bb452da2a8ec599135f2079017a84fe60be86
Sam Caldwell <github@samcaldwell.net>
2024-11-18 19:30:42 -0600

    add PdoValidatorRegex.UnmarshalYAML()

---
+++ pkg/types/manifest/PdoValidatorRegex.UnmarshalYAML.go
@@ -0,0 +1,23 @@
+package manifest
+
+import (
+	"gopkg.in/yaml.v3"
+	"regexp"
+	"strings"
+)
+
+// UnmarshalYAML - unmarshal a yaml object which is expected to contain a regular expression.
+func (p *PdoValidatorRegex) UnmarshalYAML(node *yaml.Node) error {
+	var value string
+	if err := node.Decode(&value); err != nil {
+		return err
+	}
+	re, err := regexp.Compile(strings.TrimSpace(value))
+	if err != nil {
+		return err
+	}
+	if err = p.Set(re); err != nil {
+		return err
+	}
+	return nil
+}
