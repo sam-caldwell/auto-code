31f1f6bb6e7badb9c4823901cddbb01c22208872
Sam Caldwell <github@samcaldwell.net>
2024-11-18 20:31:36 -0600

    add PdoParameterEnum and its String() method

--- pkg/types/manifest/PdoParameterEnum.String.go
+++ pkg/types/manifest/PdoParameterEnum.String.go
@@ -6,6 +6,8 @@ import (
 )
 
 // String - return a string representation of an enumerated type
+//
+// An enum is represented as follows: 'enum(v1,v2,v3)'
 func (p PdoParameterEnum) String() string {
 	return fmt.Sprintf("enum(%s)", strings.Join(p, ","))
 }
