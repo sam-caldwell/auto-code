526c733ce7e6c4d5f945792791f9edcbcbf671b2
Sam Caldwell <github@samcaldwell.net>
2024-11-19 13:05:39 -0600

    update DataSchemaDescriptorBase

--- pkg/types/manifest/DataSchemaDescriptorBase.go
+++ pkg/types/manifest/DataSchemaDescriptorBase.go
@@ -1,8 +1,10 @@
 package manifest
 
+import "github.com/sam-caldwell/auto-code/pkg/types/manifest/dataDriver"
+
 // DataSchemaDescriptorBase - a base class used to create common functionality
 //
 // This is a 'base class' per se to define common functionality.
 type DataSchemaDescriptorBase struct {
-	Driver DataDriver `yaml:"driver"`
+	Driver dataDriver.DataDriver `yaml:"driver"`
 }
