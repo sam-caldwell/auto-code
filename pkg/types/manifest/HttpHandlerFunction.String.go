36478be73a5fb93420f22e7eca9a36a1465a040d
Sam Caldwell <github@samcaldwell.net>
2024-11-19 01:20:05 -0600

    add HttpHandlerFunction and its methods

---
+++ pkg/types/manifest/HttpHandlerFunction.String.go
@@ -0,0 +1,6 @@
+package manifest
+
+// String - return a string representation of the function name
+func (h *HttpHandlerFunction) String() string {
+	return string(*h)
+}
---
+++ pkg/types/manifest/HttpHandlerFunction.go
@@ -0,0 +1,4 @@
+package manifest
+
+// HttpHandlerFunction - represents the name of the http handler function which the endpoint will invoke
+type HttpHandlerFunction string
