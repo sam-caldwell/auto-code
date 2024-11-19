52b668599d0e41e090b8919f0bb642b3ee9e8466
Sam Caldwell <github@samcaldwell.net>
2024-11-19 12:10:19 -0600

    add PostgresTableName and its methods

---
+++ pkg/types/manifest/PostgresTableName.String.go
@@ -0,0 +1,6 @@
+package manifest
+
+// String - return a string representation of PostgresTableName
+func (p *PostgresTableName) String() string {
+	return string(*p)
+}
---
+++ pkg/types/manifest/PostgresTableName.go
@@ -0,0 +1,4 @@
+package manifest
+
+// PostgresTableName - represents a standardized postgresql table name
+type PostgresTableName string
