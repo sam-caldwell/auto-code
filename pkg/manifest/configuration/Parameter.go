c57bf5367f2d7acf8d2f7cb8f2ef3ace1c151d65
Sam Caldwell <github@samcaldwell.net>
2024-11-20 15:34:05 -0600

    improve documentation for Configuraiton paramter

--- pkg/manifest/configuration/Parameter.go
+++ pkg/manifest/configuration/Parameter.go
@@ -6,8 +6,16 @@ import (
 )
 
 // Parameter - represents the top-level object in Configuration.
-// The ConfigParameters type represents the dataContract object used by the application
-// to reference fully loaded, resolved and validated configuration parameters.
+//
+// Given one or more data sources to configure a solution, the Configuration object
+// reads the configuration sources then merges them (using MergeOrder) before storing
+// and validating the final value against the Configuration.Parameter array.
+//
+// This object represents one element of the Configuration.Parameter array, and it
+// maps a parameter's name to its description and value (state and validator logic).
+//
+// When completed, the configuration process will expose the Parameter object as the
+// single source of truth.
 type Parameter struct {
 	manifest.DataObjectWithReference
 	Name        generic.Identifier      `yaml:"name"`
