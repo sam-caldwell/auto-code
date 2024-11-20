9d0495cd4202ef3663e94a4d637e9cfc8d24c2a4
Sam Caldwell <github@samcaldwell.net>
2024-11-20 14:34:59 -0600

    more cleanup from the initial poc

--- pkg/manifest/contracts/httpContract/Root.go
+++ pkg/manifest/contracts/httpContract/Root.go
@@ -2,6 +2,7 @@ package httpContract
 
 import "github.com/sam-caldwell/auto-code/pkg/manifest/artifact"
 
+// Root - defines an HTTP contract
 type Root struct {
 	Artifacts []artifact.Name          `yaml:"artifacts"`
 	Endpoints []HttpEndpointDescriptor `yaml:"endpoints"`
