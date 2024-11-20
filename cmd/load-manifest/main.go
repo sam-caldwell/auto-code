7ecf42790f2da48a5f9a4fa84e1fa6457fd7623a
Sam Caldwell <github@samcaldwell.net>
2024-11-19 22:09:09 -0600

    add cmd/load-manifest

---
+++ cmd/load-manifest/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/sam-caldwell/ansi"
+	"github.com/sam-caldwell/auto-code/pkg/config"
+	"github.com/sam-caldwell/auto-code/pkg/project"
+)
+
+/*
+ *
+ */
+func main() {
+	if config.Debug() {
+
+		ansi.Blue().Printf("starting...\n").Reset()
+
+	}
+	if err := project.Load(); err != nil {
+
+		ansi.Red().Printf("Error loading manifest: %s", err).LF().Reset()
+
+	}
+	if err := project.WriteManifest(); err != nil {
+
+		ansi.Red().Printf("Error writing manifest: %s", err).LF().Reset()
+
+	}
+}
