0e121c48390e46f23319a6417d8a471eb15d949f
Sam Caldwell <github@samcaldwell.net>
2024-11-20 13:16:39 -0600

    refactor file type for clarity

--- pkg/manifest/ArtifactOptions.go
+++ pkg/manifest/ArtifactOptions.go
@@ -1,10 +1,12 @@
 package manifest
 
+import "github.com/sam-caldwell/auto-code/pkg/types/directory"
+
 // ArtifactOptions - represents the options associated with a project artifact
 type ArtifactOptions struct {
 	DataObjectWithReference
 	Language  Language            `yaml:"language,omitempty"`
-	Sources   PosixDirectory      `yaml:"sources,omitempty"`
+	Sources   directory.Name      `yaml:"sources,omitempty"`
 	Url       UrlPattern          `yaml:"url,omitempty"`
 	Container ContainerDescriptor `yaml:"container,omitempty"`
 }
--- pkg/manifest/ConfigFile.go
+++ pkg/manifest/ConfigFile.go
@@ -1,9 +1,11 @@
 package manifest
 
+import "github.com/sam-caldwell/auto-code/pkg/types/file"
+
 // ConfigFile - represents a configuration file
 type ConfigFile struct {
 	DataObjectWithReference
-	Name   FileName         `yaml:"name"`
+	Name   file.Name        `yaml:"name"`
 	Format ConfigFileFormat `yaml:"format"`
 	Schema ConfigFileSchema `yaml:"schema"`
 }
