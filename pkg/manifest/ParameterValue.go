d182f9db05ec7178ad034639c5a474652ad17707
Sam Caldwell <github@samcaldwell.net>
2024-11-20 15:36:31 -0600

    improve documentation for ParameterValue

--- pkg/manifest/ParameterValue.go
+++ pkg/manifest/ParameterValue.go
@@ -1,6 +1,12 @@
 package manifest
 
 // ParameterValue - represents a parameter value, type and validation regex
+//
+// # This object supports external $ref references
+//
+// This object implements a ParameterDataObject (PDO) representing a strongly
+// typed data value (state) and its validator logic.  While the PDO can store
+// many different data types, it does perform type-checking and enforcement.
 type ParameterValue struct {
 	DataObjectWithReference
 	Data ParameterDataObject `yaml:"value"`
