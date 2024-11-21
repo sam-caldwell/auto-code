7b7672f954a806c14ed0f6b26f425addf41124b2
Sam Caldwell <github@samcaldwell.net>
2024-11-21 01:10:26 -0600

    add improved logging to load-manifest

--- cmd/load-manifest/main.go
+++ cmd/load-manifest/main.go
@@ -8,18 +8,15 @@ import (
 
 func main() {
 	if config.Debug() {
-
-		ansi.Blue().Printf("starting...\n").Reset()
-
+		ansi.EnableDebug()
 	}
+	ansi.Debugf("start program...").LF()
 	if err := project.Load(); err != nil {
-
-		ansi.Red().Printf("Error loading manifest: %s", err).LF().Reset()
-
+		ansi.Red().Errorf("Error loading manifest: %s", err).LF().Fatal(1)
 	}
+	ansi.Debugf("Writing manifest").LF()
 	if err := project.WriteManifest(); err != nil {
-
-		ansi.Red().Printf("Error writing manifest: %s", err).LF().Reset()
-
+		ansi.Red().Errorf("Error writing manifest: %s", err).LF().Fatal(2)
 	}
+	ansi.Debugf("Done.").LF().Reset()
 }
