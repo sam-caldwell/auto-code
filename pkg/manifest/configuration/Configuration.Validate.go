da182310253bfd73fd0827b70454801e1ac2ab04
Sam Caldwell <github@samcaldwell.net>
2024-11-20 21:22:24 -0600

    Ensure that Configuration sources are verified against the list of parameters

--- pkg/manifest/configuration/Configuration.Validate.go
+++ pkg/manifest/configuration/Configuration.Validate.go
@@ -5,17 +5,19 @@ func (c *Configuration) Validate() error {
 	if err := c.validMergeOrder(); err != nil {
 		return err
 	}
-	if err := c.validParameter(); err != nil {
-		return err
-	}
-	if err := c.validFiles(); err != nil {
-		return err
-	}
-	if err := c.validEnvironment(); err != nil {
-		return err
-	}
-	if err := c.validCommandLine(); err != nil {
-		return err
+	if count := len(c.Parameters); count == 0 {
+		if err := c.validParameter(); err != nil {
+			return err
+		}
+		if err := c.validFiles(); err != nil {
+			return err
+		}
+		if err := c.validEnvironment(); err != nil {
+			return err
+		}
+		if err := c.validCommandLine(); err != nil {
+			return err
+		}
 	}
 	return nil
 }
