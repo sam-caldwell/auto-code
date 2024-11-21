225a4f7294e9956bddf7d67c88a0720dce77d64d
Sam Caldwell <github@samcaldwell.net>
2024-11-21 01:55:21 -0600

    refactor for clarity

--- pkg/manifest/artifact/Descriptor.UnmarshalYAML.go
+++ pkg/manifest/artifact/Descriptor.UnmarshalYAML.go
@@ -1,6 +1,7 @@
 package artifact
 
 import (
+	"github.com/sam-caldwell/ansi"
 	"github.com/sam-caldwell/auto-code/pkg/types/xref"
 	"gopkg.in/yaml.v3"
 )
@@ -10,6 +11,9 @@ import (
 // This method will parse the YAML Descriptor and resolve any external $ref
 // which may be included.
 func (a *Descriptor) UnmarshalYAML(node *yaml.Node) error {
+
+	ansi.Debugln("artifact/Descriptor.UnmarshalYAML start").LF()
+	defer ansi.Debugln("artifact/Descriptor.UnmarshalYAML done").LF()
 
 	// Parse the Descriptor, handling both $ref and non-$ref dataContract
 	if err := xref.ParseYamlObjectWithReferences(node, a); err != nil {
